b99bd91841b07b16e5a85c8c6a8bb2bf531485b0
Tyler Phillips <phillity@umail.iu.edu>
2020-02-03 14:07:58 -0500

    Update startup scripts

--- data/data_setup.sh
+++ data/data_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo 'Downloading and extracting LFW features!'
+echo 'Downloading and extracting LFW and RS features!'
 
-wget https://cs.iupui.edu/~phillity/lfw_arcface_feat.npz
-wget https://cs.iupui.edu/~phillity/lfw_facenet_feat.npz
+wget https://cs.iupui.edu/~phillity/features.tar.gz
+tar -xvzf features.tar.gz
+rm features.tar.gz
 
-echo 'Downloading and extracting RS features!'
+echo 'Downloading and extracting VGGFace2 ArcFace features!'
 
-wget https://cs.iupui.edu/~phillity/rs_arcface_feat.npz
-wget https://cs.iupui.edu/~phillity/rs_facenet_feat.npz
+wget https://cs.iupui.edu/~phillity/vggface2_arcface_dataset.hdf5
+
+echo 'Downloading and extracting VGGFace2 FaceNet features!'
+
+wget https://cs.iupui.edu/~phillity/vggface2_facenet_dataset.hdf5
--- images/images_setup.sh
+++ images/images_setup.sh
@@ -5,13 +5,11 @@ echo 'Downloading and extracting LFW images!'
 wget http://vis-www.cs.umass.edu/lfw/lfw.tgz
 wget http://vis-www.cs.umass.edu/lfw/pairs.txt
 wget http://vis-www.cs.umass.edu/lfw/people.txt
-
 tar -xvzf lfw.tgz
 rm lfw.tgz
 
 echo 'Downloading and extracting RS images!'
 
 wget https://cs.iupui.edu/~phillity/rs.tar.gz
-
 tar -xvzf rs.tar.gz
 rm rs.tar.gz
