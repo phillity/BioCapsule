107f18f731ed7462d7c665c4c5625fbd46e9e3b3
Tyler Phillips <phillity@iu.edu>
2022-03-25 13:12:31 -0400

    update setup scripts

--- src/face_models/face_setup.sh
+++ src/face_models/face_setup.sh
@@ -2,18 +2,18 @@
 
 echo 'Downloading and extracting arcface model!'
 
-wget https://cs.iupui.edu/~phillity/model-r100-ii.tar.gz
+wget https://cs.iupui.edu/~xzou/Bio-Capsule-Research-2022/model-r100-ii.tar.gz
 tar -xvzf model-r100-ii.tar.gz
 rm model-r100-ii.tar.gz
 
 echo 'Downloading and extracting facenet model!'
 
-wget https://cs.iupui.edu/~phillity/model-ir-v1.tar.gz
+wget https://cs.iupui.edu/~xzou/Bio-Capsule-Research-2022/model-ir-v1.tar.gz
 tar -xvzf model-ir-v1.tar.gz
 rm model-ir-v1.tar.gz
 
 echo 'Downloading and extracting mtcnn model!'
 
-wget https://cs.iupui.edu/~phillity/model-mtcnn.tar.gz
+wget https://cs.iupui.edu/~xzou/Bio-Capsule-Research-2022/model-mtcnn.tar.gz
 tar -xvzf model-mtcnn.tar.gz
 rm model-mtcnn.tar.gz
